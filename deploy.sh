fc58da28d49ad6cb1561bb4a93166804d98d331a
faradayawerty <faradayawerty@gmail.com>
2023-06-18 05:54:42 +0300

    -

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@
 [ -n "$(command -v pacman)" ]\
 	&& sudo pacman -Sy $(grep -Po '(?<=`)([a-z0-9-]+)(?=`)' README.md)
 
-cd suckless.d && ./deploy.sh && cd ..
-cd local.d && ./deploy.sh && cd ..
-
 for i in $(ls -d unix.d/* xorg.d/*); do
 	ln -snf $(realpath $i) ~/.$(basename $i)
 done
@@ -13,4 +10,7 @@ done
 curl -fLo ~/.vim/autoload/plug.vim\
 	--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\
 	--silent
+
+cd suckless.d && ./deploy.sh && cd ..
+cd local.d && ./deploy.sh && cd ..
 
