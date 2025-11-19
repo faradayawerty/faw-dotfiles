1a3a7e36627e8249eb882014b51eb8f0e54abf82
faradayawerty <faradayawerty@gmail.com>
2025-11-19 03:18:27 +0300

    -

--- xorg.d/deploy.sh
+++ xorg.d/deploy.sh
@@ -2,6 +2,7 @@
 
 ln -snf $(realpath xinitrc) ~/.xinitrc
 ln -snf $(realpath xbindkeysrc) ~/.xbindkeysrc
+ln -snf $(realpath wallpaper.jpg) ~/.wallpaper.jpg
 
 #mkdir -p ~/.config/sxiv/exec/
 #ln -snf $(realpath sxiv_keyhandler) ~/.config/sxiv/exec/key-handler
