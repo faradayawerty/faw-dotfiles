b19eeec1af78b7e7052871db55dcd9eed37f2e2c
faradayawerty <faradayawerty@gmail.com>
2025-11-23 15:40:17 +0300

    -

--- pkg.d/deploy.sh
+++ pkg.d/deploy.sh
@@ -15,18 +15,31 @@ fi
 
 read -p "Pull fawm, fast and faxiv from github.com? [y/N] " ans
 if [ "$ans" = "y" ]; then
-	mkdir -p ~/Downloads/github.com/faradayawerty/
-	cd ~/Downloads/github.com/faradayawerty/
-	[ -d ~/Downloads/github.com/faradayawerty/fawm ] || git clone https://github.com/faradayawerty/fawm
-	cd fawm
-	sudo make clean && make && sudo make install
-	cd ..
-	[ -d ~/Downloads/github.com/faradayawerty/fast ] || git clone https://github.com/faradayawerty/fast
-	cd fast
-	sudo make clean && make && sudo make install
-	cd ..
-	[ -d ~/Downloads/github.com/faradayawerty/faxiv ] || git clone https://github.com/faradayawerty/faxiv
-	cd faxiv
-	sudo make clean && make && sudo make install
+	SRCDIR=~/.local/src/github.com/faradayawerty
+	mkdir -p $SRCDIR
+	cd $SRCDIR
+	if ! [ -d $SRCDIR/fawm ]; then
+		git clone https://github.com/faradayawerty/fawm
+		cd fawm
+		make clean && make && make install
+		cd ..
+	else
+		echo "fawm already pulled to $SRCDIR/fawm"
+	fi
+	if ! [ -d $SRCDIR/fast ]; then
+		git clone https://github.com/faradayawerty/fast
+		cd fast
+		make clean && make && make install
+		cd ..
+	else
+		echo "fast already pulled to $SRCDIR/fast"
+	fi
+	if ! [ -d $SRCDIR/faxiv ]; then
+		git clone https://github.com/faradayawerty/faxiv
+		cd faxiv
+		make clean && make && make install
+	else
+		echo "faxiv already pulled to $SRCDIR/faxiv"
+	fi
 fi
 
