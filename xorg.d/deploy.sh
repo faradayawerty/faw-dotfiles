acce1a3c6095900ad1c1c74640615d3c93c59e7b
faradayawerty <faradayawerty@gmail.com>
2025-12-04 17:10:35 +0300

    -

--- xorg.d/deploy.sh
+++ xorg.d/deploy.sh
@@ -3,9 +3,11 @@
 distro="$@"
 
 mkdir -p ~/.config/sxiv/exec/
+mkdir -p ~/Pictures/
 
 ln -snf $(realpath xinitrc) ~/.xinitrc
 ln -snf $(realpath xbindkeysrc) ~/.xbindkeysrc
 ln -snf $(realpath wallpaper.jpg) ~/.wallpaper.jpg
 ln -snf $(realpath sxiv_keyhandler) ~/.config/sxiv/exec/key-handler
+ln -snf $(realpath wallpapers) ~/Pictures/wallpapers
 
