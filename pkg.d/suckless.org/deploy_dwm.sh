867642ad6850eed30a849524b94e3b1b1d611717
faradayawerty <faradayawerty@gmail.com>
2025-12-25 22:44:53 +0300

    -

--- pkg.d/suckless.org/deploy_dwm.sh
+++ pkg.d/suckless.org/deploy_dwm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 [ -d ~/.local/share/dwm-faw-patch ]\
-	&& echo dwm already deployed at ~/.local/share/dwm-faw-patch\
-	&& exit
+	&& rm -r ~/.local/share/dwm-faw-patch
 
 patch_path=$(realpath dwm-faw-patch.diff)\
 	&& mkdir -p ~/.local/share/dwm-faw-patch/\
 	&& cd ~/.local/share/dwm-faw-patch/\
 	&& aria2c https://dl.suckless.org/dwm/dwm-6.6.tar.gz\
 	&& tar xf dwm-6.6.tar.gz\
-	&& cd dwm-6.6\
+	&& cp -r dwm-6.6 dwm-6.6-patched\
+	&& cd dwm-6.6-patched\
 	&& patch -p1 < $patch_path\
 	&& make\
 	&& make install
--- pkg.d/suckless.org/deploy_st.sh
+++ pkg.d/suckless.org/deploy_st.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 [ -d ~/.local/share/st-faw-patch ]\
-	&& echo st already deployed at ~/.local/share/st-faw-patch\
-	&& exit
+	&& rm -r ~/.local/share/st-faw-patch
 
 patch_path=$(realpath st-faw-patch.diff)\
 	&& mkdir -p ~/.local/share/st-faw-patch/\
 	&& cd ~/.local/share/st-faw-patch/\
 	&& aria2c https://dl.suckless.org/st/st-0.9.3.tar.gz\
 	&& tar xf st-0.9.3.tar.gz\
+	&& cp -r st-0.9.3 st-0.9.3-patched\
 	&& cd st-0.9.3\
 	&& patch -p1 < $patch_path\
 	&& make\
