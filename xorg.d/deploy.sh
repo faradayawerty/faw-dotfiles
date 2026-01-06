bf44f43fc6c0a5050ad49350effdacce39986d5d
faradayawerty <faradayawerty@gmail.com>
2026-01-06 20:11:07 +0300

    -

--- xorg.d/deploy.sh
+++ xorg.d/deploy.sh
@@ -7,7 +7,6 @@ mkdir -p ~/Pictures/
 
 ln -snf $(realpath xinitrc) ~/.xinitrc
 ln -snf $(realpath xbindkeysrc) ~/.xbindkeysrc
-ln -snf $(realpath wallpaper.jpg) ~/.wallpaper.jpg
 ln -snf $(realpath sxiv_keyhandler) ~/.config/sxiv/exec/key-handler
 ln -snf $(realpath wallpapers) ~/Pictures/wallpapers
 
