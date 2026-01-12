7af56a7f1c0dbf550ce2e1a2827b6d35167d3964
faradayawerty <faradayawerty@gmail.com>
2026-01-12 15:45:43 +0300

    -

--- pkg.d/suckless.org/deploy_dwm.sh
+++ pkg.d/suckless.org/deploy_dwm.sh
@@ -6,10 +6,10 @@
 patch_path=$(realpath dwm-faw-patch.diff)\
 	&& mkdir -p ~/.local/share/dwm-faw-patch/\
 	&& cd ~/.local/share/dwm-faw-patch/\
-	&& aria2c https://dl.suckless.org/dwm/dwm-6.6.tar.gz\
-	&& tar xf dwm-6.6.tar.gz\
-	&& cp -r dwm-6.6 dwm-6.6-patched\
-	&& cd dwm-6.6-patched\
+	&& aria2c https://dl.suckless.org/dwm/dwm-6.7.tar.gz\
+	&& tar xf dwm-6.7.tar.gz\
+	&& cp -r dwm-6.7 dwm-6.7-patched\
+	&& cd dwm-6.7-patched\
 	&& patch -p1 < $patch_path\
 	&& make\
 	&& make install\
